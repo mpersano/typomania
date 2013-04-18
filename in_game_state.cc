1758c5b22a904d58f34f06120e54d95ab5a427e4
fzort <fzort@tycho.luna>
2013-04-18 01:35:37 -0300

    Added kana/romaji table.

--- in_game_state.cc
+++ in_game_state.cc
@@ -8,6 +8,28 @@
 
 static const char *STREAM_DIR = "data/streams";
 
+static const struct kana_to_romaji {
+	const wchar_t kana;
+	const char *romaji;
+} kana_to_romaji[] = {
+	{ L'あ', "a"  }, { L'い', "i"  }, { L'う', "u"  }, { L'え', "e"  }, { L'お', "o"  },
+	{ L'か', "ka" }, { L'き', "ki" }, { L'く', "ku" }, { L'け', "ke" }, { L'こ', "ko" },
+	{ L'さ', "sa" }, { L'し', "si" }, { L'す', "su" }, { L'せ', "se" }, { L'そ', "so" },
+	{ L'た', "ta" }, { L'ち', "ti" }, { L'つ', "tu" }, { L'て', "te" }, { L'と', "to" },
+	{ L'な', "na" }, { L'に', "ni" }, { L'ぬ', "nu" }, { L'ね', "ne" }, { L'の', "no" },
+	{ L'は', "ha" }, { L'ひ', "hi" }, { L'ふ', "fu" }, { L'へ', "he" }, { L'ほ', "ho" },
+	{ L'ま', "ma" }, { L'み', "mi" }, { L'む', "mu" }, { L'め', "me" }, { L'も', "mo" },
+	{ L'や', "ya" }, { L'ゆ', "yu" }, { L'よ', "yo" },
+	{ L'ら', "ra" }, { L'り', "ri" }, { L'る', "ru" }, { L'れ', "re" }, { L'ろ', "ro" },
+	{ L'わ', "wa" }, { L'を', "wo" },
+	{ L'が', "ga" }, { L'ぎ', "gi" }, { L'ぐ', "gu" }, { L'げ', "ge" }, { L'ご', "go" },
+	{ L'ざ', "za" }, { L'じ', "ji" }, { L'ず', "zu" }, { L'ぜ', "ze" }, { L'ぞ', "zo" },
+	{ L'だ', "da" }, { L'ぢ', "di" }, { L'づ', "du" }, { L'で', "de" }, { L'ど', "do" },
+	{ L'ば', "ba" }, { L'び', "bi" }, { L'ぶ', "bu" }, { L'べ', "be" }, { L'ぼ', "bo" },
+	{ L'ぱ', "pa" }, { L'ぴ', "pi" }, { L'ぷ', "pu" }, { L'ぺ', "pe" }, { L'ぽ', "po" },
+	{ 0, 0 },
+};
+
 in_game_state::in_game_state(const kashi& cur_kashi)
 : cur_kashi(cur_kashi)
 , spectrum(player)
@@ -72,8 +94,10 @@ in_game_state::draw_serifu() const
 
 	const kashi::serifu& serifu = *cur_serifu;
 
-	const float x0 = 8, x1 = WINDOW_WIDTH - 8, xm = x0 + ((x1 - x0)*cur_serifu_ms/serifu.duration);
+	const float x0 = 8, x1 = WINDOW_WIDTH - 8, xm = x0 + (x1 - x0)*cur_serifu_ms/serifu.duration;
 	const float y0 = 60, y1 = 65;
+
+	glDisable(GL_TEXTURE_2D);
 
 	glBegin(GL_QUADS);
 
@@ -98,22 +122,15 @@ in_game_state::draw_serifu() const
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	glEnable(GL_TEXTURE_2D);
+
 	gv.reset();
 	gv.add_string(tiny_font, &serifu.kana[0], 10, 32);
-
-	glEnable(GL_TEXTURE_2D);
 	tiny_font->texture->bind();
-
 	gv.draw(GL_QUADS);
 
 	gv.reset();
 	gv.add_string(small_font, &serifu.kanji[0], 10, 10);
-
-	glEnable(GL_TEXTURE_2D);
 	small_font->texture->bind();
-
 	gv.draw(GL_QUADS);
-
-	glDisable(GL_TEXTURE_2D);
-	glDisable(GL_BLEND);
 }
