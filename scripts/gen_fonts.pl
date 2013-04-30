c38ae2d8da2363c0710ccc308acf52f430fb7897
fzort <mauro.persano@ti0072l.ortizgaming.local>
2013-04-30 12:45:30 -0300

    Only adding used glyphs to font.

--- scripts/gen_fonts.pl
+++ scripts/gen_fonts.pl
@@ -11,25 +11,28 @@ use constant {
 
 binmode STDOUT, ':utf8';
 
-my @glyphs = qw(32-126 12448-12543 12352-12447 12288 12293 65281-65374);
-# my @glyphs = qw(32-126 12448-12543 12352-12447 12288 12293);
+my %glyphs;
 
-my %kanji;
+$glyphs{$_} = 1 for 32 .. 126;
 
 for my $kashi (<../data/lyrics/*kashi>) {
 	open IN, $kashi;
 	binmode IN, ':utf8';
 	
 	while (<IN>) {
-		$kanji{$1} = 1 while /([\x{4e00}-\x{9fbf}])/g;
+		for (split //) {
+			if ($_ !~ /[\t\n\r]/) {
+				$glyphs{ord($_)} = 1;
+			}
+		}
 	}
 	
 	close IN;
 }
 
-push @glyphs, (map { ord } keys %kanji);
+my @glyphs = keys %glyphs;
 
 system DUMPGLYPHS, '-W', 512, '-H', 512, '-S', 10, '-I', 'tiny', '-p', IMAGE_PATH, FONT, @glyphs;
-system DUMPGLYPHS, '-W', 512, '-H', 1024, '-S', 18, '-I', 'small', '-p', IMAGE_PATH, FONT, @glyphs;
-system DUMPGLYPHS, '-W', 1024, '-H', 1024, '-S', 24, '-I', 'medium', '-p', IMAGE_PATH, FONT, @glyphs;
+system DUMPGLYPHS, '-W', 512, '-H', 512, '-S', 18, '-I', 'small', '-p', IMAGE_PATH, FONT, @glyphs;
+system DUMPGLYPHS, '-W', 512, '-H', 1024, '-S', 24, '-I', 'medium', '-p', IMAGE_PATH, FONT, @glyphs;
 system DUMPGLYPHS, '-W', 256, '-H', 256, '-S', 36, '-I', 'big_az', '-p', IMAGE_PATH, FONT, ord('A').'-'.ord('Z'), ord('-');
