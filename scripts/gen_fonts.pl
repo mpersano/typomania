52953f699a06547e7a3932eea65e992393dcd17b
fzort <fzort@tycho.luna>
2013-05-11 10:31:11 -0300

    Added more lyrics.

--- scripts/gen_fonts.pl
+++ scripts/gen_fonts.pl
@@ -34,6 +34,6 @@ my @glyphs = keys %glyphs;
 
 system DUMPGLYPHS, '-W', 512, '-H', 512, '-S', 10, '-I', 'tiny', '-p', IMAGE_PATH, FONT, @glyphs;
 system DUMPGLYPHS, '-W', 512, '-H', 1024, '-S', 18, '-I', 'small', '-p', IMAGE_PATH, FONT, @glyphs;
-system DUMPGLYPHS, '-W', 512, '-H', 1024, '-S', 24, '-I', 'medium', '-p', IMAGE_PATH, FONT, @glyphs;
+system DUMPGLYPHS, '-W', 1024, '-H', 1024, '-S', 24, '-I', 'medium', '-p', IMAGE_PATH, FONT, @glyphs;
 system DUMPGLYPHS, '-W', 256, '-H', 256, '-S', 36, '-I', 'big_az', '-p', IMAGE_PATH, FONT,
   ord('A').'-'.ord('Z'), ord('0').'-'.ord('9'), ord('-');
