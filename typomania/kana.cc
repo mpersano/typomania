89c242584d86d2d08cc86b879516f94d16fc24d4
Mauro Persano <mauro.persano@kdab.com>
2016-11-18 18:57:12 -0200

    some refactoring

--- typomania/kana.cc
+++ typomania/kana.cc
@@ -8,22 +8,7 @@ namespace kana {
 
 namespace {
 
-class kana_to_pattern
-{
-public:
-	static kana_to_pattern& get_instance();
-
-	const pattern_node *find_pattern(wchar_t kana);
-	const pattern_node *find_pattern(wchar_t kana0, wchar_t kana1);
-	const pattern_node *find_pattern(wchar_t kana0, wchar_t kana1, wchar_t kana2);
-
-private:
-	kana_to_pattern();
-
-	std::map<wchar_t, std::shared_ptr<pattern_node>> kana_to_pattern_;
-	std::map<std::pair<wchar_t, wchar_t>, std::shared_ptr<pattern_node>> kana_pair_to_pattern_;
-	std::map<std::tuple<wchar_t, wchar_t, wchar_t>, std::shared_ptr<pattern_node>> kana_triple_to_pattern_;
-};
+using pattern_ptr = std::shared_ptr<pattern_node>;
 
 wchar_t
 hira_to_kata(wchar_t ch)
@@ -37,9 +22,11 @@ half_to_full(wchar_t ch)
 	return ch - L'a' + L'ａ';
 }
 
-kana_to_pattern::kana_to_pattern()
+std::map<wchar_t, pattern_ptr> init_kana_to_pattern_map()
 {
-	static const std::vector<std::pair<wchar_t, const char *>> kana_to_romaji
+	std::map<wchar_t, pattern_ptr> map;
+
+	static const std::pair<wchar_t, const char *> kana_to_romaji[]
 		{ { L'あ', "A"  }, { L'い', "I"  }, { L'う', "U"  }, { L'え', "E"  }, { L'お', "O"  },
 		  { L'か', "KA" }, { L'き', "KI" }, { L'く', "KU" }, { L'け', "KE" }, { L'こ', "KO" },
 		  { L'さ', "SA" }, { L'し', "SH?I" }, { L'す', "SU" }, { L'せ', "SE" }, { L'そ', "SO" },
@@ -60,37 +47,44 @@ kana_to_pattern::kana_to_pattern()
 		  { L'っ', "[LX]TS?U" } };
 
 	for (auto& p : kana_to_romaji) {
-		std::shared_ptr<pattern_node> pattern(parse_pattern(p.second));
+		pattern_ptr pattern(parse_pattern(p.second));
 
-		kana_to_pattern_[p.first] = pattern;
-		kana_to_pattern_[hira_to_kata(p.first)] = pattern;
+		map.insert(std::make_pair(p.first, pattern));
+		map.insert(std::make_pair(hira_to_kata(p.first), pattern));
 	}
 
 	for (char i = 'A'; i <= 'Z'; i++) {
 		char pattern_str[] { i, '\0' };
-		std::shared_ptr<pattern_node> pattern(parse_pattern(pattern_str));
+		pattern_ptr pattern(parse_pattern(pattern_str));
 
-		kana_to_pattern_[i] = pattern;
-		kana_to_pattern_[half_to_full(i)] = pattern;
+		map.insert(std::make_pair(i, pattern));
+		map.insert(std::make_pair(half_to_full(i), pattern));
 	}
 
 	for (char i = 'a'; i <= 'z'; i++) {
 		char pattern_str[] { static_cast<char>(i - 'a' + 'A'), '\0' };
-		std::shared_ptr<pattern_node> pattern(parse_pattern(pattern_str));
+		pattern_ptr pattern(parse_pattern(pattern_str));
 
-		kana_to_pattern_[i] = pattern;
-		kana_to_pattern_[half_to_full(i)] = pattern;
+		map.insert(std::make_pair(i, pattern));
+		map.insert(std::make_pair(half_to_full(i), pattern));
 	}
 
 	for (char i = '0'; i <= '9'; i++) {
 		char pattern_str[] { i, '\0' };
-		std::shared_ptr<pattern_node> pattern(parse_pattern(pattern_str));
+		pattern_ptr pattern(parse_pattern(pattern_str));
 
-		kana_to_pattern_[i] = pattern;
-		kana_to_pattern_[half_to_full(i)] = pattern;
+		map.insert(std::make_pair(i, pattern));
+		map.insert(std::make_pair(half_to_full(i), pattern));
 	}
 
-	static const std::vector<std::pair<const wchar_t *, const char *>> kana_pair_to_romaji
+	return map;
+}
+
+std::map<std::pair<wchar_t, wchar_t>, pattern_ptr> init_kana_pair_to_pattern_map()
+{
+	std::map<std::pair<wchar_t, wchar_t>, pattern_ptr> map;
+
+	static const std::pair<const wchar_t *, const char *> kana_pair_to_romaji[]
 		{ { L"きゃ", "KYA" }, { L"きゅ", "KYU" }, { L"きょ", "KYO" },
 		  { L"しゃ", "S[YH]A" }, { L"しゅ", "S[YH]U" }, { L"しょ", "S[YH]O" },
 		  { L"ちゃ", "[TC][YH]A" }, { L"ちゅ", "[TC][YH]U" }, { L"ちょ", "[TC][YH]O" },
@@ -108,15 +102,21 @@ kana_to_pattern::kana_to_pattern()
 		  { L"っぱ", "PPA" }, { L"っぴ", "PPI" }, { L"っぷ", "PPU" }, { L"っぺ", "PPE" }, { L"っぽ", "PPO" } };
 
 	for (auto& p : kana_pair_to_romaji) {
-		std::shared_ptr<pattern_node> pattern(parse_pattern(p.second));
+		pattern_ptr pattern(parse_pattern(p.second));
 
 		auto kana = p.first;
-
-		kana_pair_to_pattern_[std::make_pair(kana[0], kana[1])] = pattern;
-		kana_pair_to_pattern_[std::make_pair(hira_to_kata(kana[0]), hira_to_kata(kana[1]))] = pattern;
+		map.insert(std::make_pair(std::make_pair(kana[0], kana[1]), pattern));
+		map.insert(std::make_pair(std::make_pair(hira_to_kata(kana[0]), hira_to_kata(kana[1])), pattern));
 	}
 
-	static const std::vector<std::pair<const wchar_t *, const char *>> kana_triple_to_romaji
+	return map;
+}
+
+std::map<std::tuple<wchar_t, wchar_t, wchar_t>, pattern_ptr> init_kana_triple_to_pattern_map()
+{
+	std::map<std::tuple<wchar_t, wchar_t, wchar_t>, pattern_ptr> map;
+
+	static const std::pair<const wchar_t *, const char *> kana_triple_to_romaji[]
 		{ { L"っきゃ", "KKYA" }, { L"っきゅ", "KKYU" }, { L"っきょ", "KKYO" },
 		  { L"っしゃ", "SS[YH]A" }, { L"っしゅ", "SS[YH]U" }, { L"っしょ", "SS[YH]O" },
 		  { L"っちゃ", "[TC][TC][YH]A" }, { L"っちゅ", "[TC][TC][YH]U" }, { L"っちょ", "[TC][TC][YH]O" },
@@ -129,42 +129,14 @@ kana_to_pattern::kana_to_pattern()
 		  { L"っぴゃ", "PPYA" }, { L"っぴゅ", "PPYU" }, { L"っぴょ", "PPYO" } };
 
 	for (auto& p : kana_triple_to_romaji) {
-		std::shared_ptr<pattern_node> pattern(parse_pattern(p.second));
+		pattern_ptr pattern(parse_pattern(p.second));
 
 		auto kana = p.first;
-
-		kana_triple_to_pattern_[std::make_tuple(p.first[0], p.first[1], p.first[2])] = pattern;
-		kana_triple_to_pattern_[std::make_tuple(hira_to_kata(p.first[0]), hira_to_kata(p.first[1]), hira_to_kata(p.first[2]))] = pattern;
+		map.insert(std::make_pair(std::make_tuple(kana[0], kana[1], kana[2]), pattern));
+		map.insert(std::make_pair(std::make_tuple(hira_to_kata(kana[0]), hira_to_kata(kana[1]), hira_to_kata(kana[2])), pattern));
 	}
 
-}
-
-kana_to_pattern&
-kana_to_pattern::get_instance()
-{
-	static kana_to_pattern instance;
-	return instance;
-}
-
-const pattern_node *
-kana_to_pattern::find_pattern(wchar_t kana0, wchar_t kana1, wchar_t kana2)
-{
-	auto it = kana_triple_to_pattern_.find(std::make_tuple(kana0, kana1, kana2));
-	return it != kana_triple_to_pattern_.end() ? it->second.get() : nullptr;
-}
-
-const pattern_node *
-kana_to_pattern::find_pattern(wchar_t kana0, wchar_t kana1)
-{
-	auto it = kana_pair_to_pattern_.find(std::make_pair(kana0, kana1));
-	return it != kana_pair_to_pattern_.end() ? it->second.get() : nullptr;
-}
-
-const pattern_node *
-kana_to_pattern::find_pattern(wchar_t kana)
-{
-	auto it = kana_to_pattern_.find(kana);
-	return it != kana_to_pattern_.end() ? it->second.get() : nullptr;
+	return map;
 }
 
 }
@@ -172,22 +144,28 @@ kana_to_pattern::find_pattern(wchar_t kana)
 const pattern_node *
 find_pattern(wchar_t kana)
 {
-	kana_to_pattern& instance = kana_to_pattern::get_instance();
-	return instance.find_pattern(kana);
+	static const std::map<wchar_t, pattern_ptr> map(init_kana_to_pattern_map());
+
+	auto it = map.find(kana);
+	return it != map.end() ? it->second.get() : nullptr;
 }
 
 const pattern_node *
 find_pattern(wchar_t kana0, wchar_t kana1)
 {
-	kana_to_pattern& instance = kana_to_pattern::get_instance();
-	return instance.find_pattern(kana0, kana1);
+	static const std::map<std::pair<wchar_t, wchar_t>, pattern_ptr> map(init_kana_pair_to_pattern_map());
+
+	auto it = map.find(std::make_pair(kana0, kana1));
+	return it != map.end() ? it->second.get() : nullptr;
 }
 
 const pattern_node *
 find_pattern(wchar_t kana0, wchar_t kana1, wchar_t kana2)
 {
-	kana_to_pattern& instance = kana_to_pattern::get_instance();
-	return instance.find_pattern(kana0, kana1, kana2);
+	static const std::map<std::tuple<wchar_t, wchar_t, wchar_t>, pattern_ptr> map(init_kana_triple_to_pattern_map());
+
+	auto it = map.find(std::make_tuple(kana0, kana1, kana2));
+	return it != map.end() ? it->second.get() : nullptr;
 }
 
 }
